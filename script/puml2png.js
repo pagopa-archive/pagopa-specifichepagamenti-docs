08b75fca3311499ee0630537706d6d1da067ba33
gammam <m.gammaldi@gmail.com>
2021-09-16 10:28:56 +0200

    (fix) create folder if not exist

--- script/puml2png.js
+++ script/puml2png.js
@@ -5,12 +5,10 @@ const fs = require('fs');
 //joining path of directory 
 const  __rootDir = path.dirname(__dirname,'..');
 const directoryPath = path.join(__rootDir, 'puml-diagrams');
-// const outDirectoryPath = path.join(__rootDir, '_docs','diagrams');
+
 const outDirectoryPathMD = path.join(__rootDir,'diagrams');
 
-if (!fs.existsSync(outDirectoryPath)) {
-    fs.mkdirSync(outDirectoryPath,{recursive:true});
-}
+
 
 
 //passsing directoryPath and callback function
